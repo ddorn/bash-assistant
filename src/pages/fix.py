0c88b7e9d0b27da26fdd4a80386f286018546330
ddorn <diego.dorn@free.fr>
2024-06-12 01:43:39 +0200

    feat(fix):  Add system prompts and word-level diff toggles
    Refactor the text fixing interface to allow for different system prompts including custom ones. Added a new word-level toggle feature for visualizing text differences. Improved the HTML formatting for displaying text differences.

--- src/pages/fix.py
+++ src/pages/fix.py
@@ -1,31 +1,40 @@
+from html import escape
 import difflib
 import re
+from textwrap import dedent
 import streamlit as st
+from streamlit.components.v1 import html
 
 from utils import ai_stream
 
 
-text = st.text_area("Text to fix")
-heavy = st.checkbox("Also reformulate")
+with st.container(border=True):
+    text = st.text_area("Text to fix")
 
+    system_prompts = {
+        "Default": """
+    You are given a text and you need to fix the language (typos, grammar, ...).
+    If needed, fix also the formatting and ensure the text is gender neutral.
+    Output directly the corrected text, without any comment.
+    """.strip(),
+        "Heavy": """
+    You are given a text and you need to fix the language (typos, grammar, ...).
+    If needed, fix also the formatting and ensure the text is gender neutral.
+    "Please reformulate the text when needed, use better words and make it more clear when possible.",
+    Output directly the corrected text, without any comment.
+    """.strip(),
+        "Custom": "",
+    }
 
-def fix(text, heavy):
-    system = """
-You are given a text and you need to fix the language (typos, grammar, ...).
-If needed, fix also the formatting and ensure the text is gender neutral. HEAVY
-Output directly the corrected text, without any comment.
-""".strip()
-
-    if heavy:
-        system = system.replace(
-            "HEAVY",
-            "Please reformulate the text when needed, use better words and make it more clear when possible.",
-        )
+    # Allow for custom prompts also
+    system_name = st.selectbox("System Prompt", list(system_prompts.keys()))
+    if system_name == "Custom":
+        system = st.text_area("Custom prompt", value=system_prompts["Default"])
     else:
-        system = system.replace(" HEAVY", "")
+        system = system_prompts[system_name]
+        st.markdown(system)
 
-    result = st.write_stream(ai_stream(system, [dict(role="user", content=text)]))
-    return result
+    lets_gooo = st.button("Fix", type="primary")
 
 
 @st.cache_resource()
@@ -33,16 +42,24 @@ def cache():
     return {}
 
 
+if lets_gooo:
+    corrected = st.write_stream(ai_stream(system, [dict(role="user", content=text)]))
+    cache()[text, system] = corrected
+    st.rerun()
+else:
+    corrected = cache().get((text, system))
+
+
 def fmt_diff_html(diff: list[str], select_new: bool) -> str:
     no_select = "user-select: none; color: gray;"
 
     colored = ""
     for word in diff:
-        kind = word[0]
+        kind = escape(word[0])
         word = word[2:]
 
         if kind == " ":
-            colored += word
+            colored += f"<span>{word}</span>"
             continue
         elif kind == "?":
             continue
@@ -63,28 +80,120 @@ def fmt_diff_html(diff: list[str], select_new: bool) -> str:
             continue
         colored += "'>" + word + "</span>"
 
-    return colored
+    return colored.replace("\n", "<br>")
 
 
-if st.button("Fix"):
-    corrected = fix(text, heavy)
-    cache()[text, heavy] = corrected
-    st.rerun()
-else:
-    corrected = cache().get((text, heavy))
+def fmt_diff_toggles(diff: list[str]) -> str:
+    start = dedent(
+        """
+    <style>
+        .swaper:checked + label .new {
+            user-select: none;
+            color: gray;
+            border: 1px dashed;
+        }
+        .swaper:not(:checked) + label .original {
+            user-select: none;
+            color: gray;
+            border: 1px dashed;
+        }
+        .swapable {
+            border: 1px solid;
+            padding: 2px;
+            min-width: 20px;
+            white-space: pre;
+        }
+        .original {
+            border-color: red;
+            background-color: rgba(255, 0, 0, 0.05);
+            border-radius: 5px 0 0 5px;
+            text-decoration-color: red;
+        }
+        .new {
+            border-color: green;
+            background-color: rgba(0, 255, 0, 0.05);
+            border-radius: 0 5px 5px 0;
+            text-decoration-color: green;
+        }
+        .swapable-label {
+            display: inline-block;
+            cursor: grab;
+        }
+    </style>"""
+    )
+
+    template = """
+<input type="checkbox" style="display: none;" class="swaper" id={id}>
+<label for={id} style="swapable-label">
+    <span class="swapable original">{content1}</span><span class="swapable new">{content2}</span>
+</label>"""
+
+    # Diff always outputs "- old" then "+ new" word, but both can be empty
+    parts: list[str | tuple[str, str]] = []
+
+    for word in diff:
+        kind = word[0]
+        word = escape(word[2:])
+
+        if kind == " ":
+            if parts and isinstance(parts[-1], str):
+                parts[-1] += word
+            else:
+                parts.append(word)
+        elif kind == "?":
+            continue
+        elif kind == "-":
+            # if parts and isinstance(parts[-1], tuple):
+            #     if parts[-1][1] == "":
+            #         parts[-1] = (parts[-1][0] + word, "")
+            #     else:
+            #         parts.append((word, ""))
+            # else:
+            parts.append((word, ""))
+        elif kind == "+":
+            # if parts and isinstance(parts[-1], tuple):
+            #     parts[-1] = (parts[-1][0], word)
+            # else:
+            if parts and isinstance(parts[-1], tuple) and parts[-1][1] == "":
+                parts[-1] = (parts[-1][0], word)
+            else:
+                parts.append(("", word))
+        else:
+            raise ValueError(f"Unknown kind: {kind}")
+
+    # Escape the text
+
+    colored = start
+    for i, part in enumerate(parts):
+        if isinstance(part, tuple):
+            colored += template.format(id=i, content1=part[0], content2=part[1])
+        else:
+            colored += f"<span>{part}</span>"
+
+    return f"<p>{colored}</p>"
+
+
+def split_words(text: str) -> list[str]:
+    # Also split on newlines
+    words = re.findall(r"(\w+|\W+|\n+)", text.strip())
+    return words
+
 
 if corrected is not None:
+    st.write(
+        "*:green[Green text] is the corrected version, :red[red text] is the original version.*"
+    )
+
     # Compute the difference between the two texts
-    words1 = re.findall(r"(\w+|\W+)", text.strip())
-    words2 = re.findall(r"(\w+|\W+)", corrected.strip())
+    words1 = split_words(text)
+    words2 = split_words(corrected)
 
     diff = list(difflib.ndiff(words1, words2))
-    # past, corrected = utilfmt_diff(diff)
 
-    st.header("Corrected text")
-    colored = fmt_diff_html(diff, select_new=True)
-    st.write(colored, unsafe_allow_html=True)
+    st.header("Word level toggles")
+    st.html(fmt_diff_toggles(diff))
 
-    st.header("Old text")
-    colored = fmt_diff_html(diff, select_new=False)
-    st.write(colored, unsafe_allow_html=True)
+    st.header("Global toggle")
+    select_old = st.checkbox("Make old text selectable", value=False)
+    colored = fmt_diff_html(diff, select_new=not select_old)
+    st.html(colored)
